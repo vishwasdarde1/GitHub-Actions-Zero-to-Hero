9ada1b79224dce1137d73a8e046f5e9b3482a203
vishwas <vishwasdarde1432@gmail.com>
2026-02-11 15:56:17 +0530

    commited 5

--- src/addition.py
+++ src/addition.py
@@ -1,9 +1,6 @@
 # app.py
-<<<<<<< HEAD
 # This is a test commit by vishwas3
-=======
 # This is a test commit by vishwas2
->>>>>>> 4d51172cd2cbe1f0e2a1cd618386af0c6e562faa
 def add(a, b):
     return a * b
 

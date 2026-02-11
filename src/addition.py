e0bab288b1489583588c680077f56efeeb6aa59b
vishwas <vishwasdarde1432@gmail.com>
2026-02-11 16:09:31 +0530

    new commit 5

--- src/addition.py
+++ src/addition.py
@@ -2,8 +2,9 @@
 # This is a test commit by vishwas3
 # This is a test commit by vishwas2
 def add(a, b):
-    return a * b
+    return a + b
 
 def test_add():
-    assert add(1, 2) == 4
-    assert add(1, -1) == 0
+    assert add(5, 7) == 12
+    assert add(-3, 3) == 0
+    assert add(10, -4) == 6
